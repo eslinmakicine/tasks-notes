7fdd1cb0a747aeb2f50635c6c082e836501034bc
eslinmakicine <eslinmartins@gmail.com>
2020-01-17 18:06:20 -0300

    Atualizando para js puro, organizando o código e adc funcionalidades de adc tarefas, remover tarefas

--- js/adicionarTarefa.js
+++ js/adicionarTarefa.js
@@ -1,13 +1,57 @@
-var botaoAdicionar = $("#btn-adicionar");
-var formTarefa = $("form-adicionar");
-
-botaoAdicionar.on("click", function() {
-    console.log(botaoAdicionar);
-
-    (botaoAdicionar).addClass("btn-esconder");
-    (formTarefa).removeClass("form-esconder");
-
-});
+var botaoAdicionar = document.querySelector("#btn-adicionar");
+var formTarefa = document.querySelector("#form-adicionar");
+var listaTarefaPend = document.querySelector("#lista-tarefas-pendentes");
+var listaTarefaConc = document.querySelector(".lista-tarefas-concluidas");
 
 
+
+ocultarBotao(botaoAdicionar, formTarefa);
+adicionarTarefa(formTarefa, botaoAdicionar, listaTarefaPend);
+removerPendente(listaTarefaPend);
+
+
+
+
+function adicionarTarefa(formTarefa, botaoAdicionar, listaTarefaPend) {
+
+
+    formTarefa.addEventListener("keyup", function () {
+        if (event.keyCode === 13) {
+
+            var txtTarefa = formTarefa.value;
+
+            var listaTarefaPendLi = document.createElement("li");
+            listaTarefaPendLi.textContent = txtTarefa;
+            listaTarefaPend.appendChild(listaTarefaPendLi);
+            listaTarefaPendLi.classList.add("tarefa-pendente");
+            botaoAdicionar.classList.remove("btn-esconder");
+            formTarefa.classList.add("form-esconder");
+            document.getElementById('form-adicionar').value = "";
+        }
+    });
+}
+
+function ocultarBotao(botaoAdicionar, formTarefa) {
+    botaoAdicionar.addEventListener("click", function () {
+        botaoAdicionar.classList.add("btn-esconder");
+        formTarefa.classList.remove("form-esconder");
+    });
+}
+
+
+
+function removerPendente(listaTarefaPend) {
+    listaTarefaPend.addEventListener('dblclick', function (e) {
+
+        if (e.target && e.target.classList.contains('tarefa-pendente')) {
+
+            alert(e.target);
+        }
+    }, false);
+}
+
+function moverConcluida() {
+
+
+}
 
